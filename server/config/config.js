2f0d310a25d6ba61f45ccaa02f083dc787621e26
Kevin <kevcabanar@gmail.com>
2020-03-18 18:00:58 -0500

    2

--- server/config/config.js
+++ server/config/config.js
@@ -15,9 +15,11 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://kevin:Pruebas123@cafe-htqjd.mongodb.net/test?retryWrites=true&w=majority';
+    //Desarrollado en MongoDB atlas la nueva que absorbio a mlab
+    // urlDB = 'mongodb+srv://kevin:Pruebas123@cafe-htqjd.mongodb.net/test?retryWrites=true&w=majority';
 
-    // urlDB = 'mongodb://cafe-user:Pruebas123@ds331145.mlab.com:31145/heroku_28jfstht';
+    //desarrollado con mlab su pagina antigua que esta anexada con heroku 
+    urlDB = 'mongodb://cafe-user:Pruebas123@ds331145.mlab.com:31145/heroku_28jfstht';
     // urlDB = process.env.MONGO_URI;
 }
 

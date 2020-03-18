ca654d307f398979dfac823e9d89e58cf3acdfc8
Kevin <kevcabanar@gmail.com>
2020-03-18 18:15:37 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,8 +19,11 @@ if (process.env.NODE_ENV === 'dev') {
     // urlDB = 'mongodb+srv://kevin:Pruebas123@cafe-htqjd.mongodb.net/test?retryWrites=true&w=majority';
 
     //desarrollado con mlab su pagina antigua que esta anexada con heroku 
-    urlDB = 'mongodb://cafe-user:Pruebas123@ds331145.mlab.com:31145/heroku_28jfstht';
-    // urlDB = process.env.MONGO_URI;
+    // urlDB = 'mongodb://cafe-user:Pruebas123@ds331145.mlab.com:31145/heroku_28jfstht';
+    urlDB = process.env.MONGO_URI;
+    //se crea en heroku process.env.MONGO_URI
+    //heroku config:set MONGO_URI="mongodb://cafe-user:Pruebas123@ds331145.mlab.com:31145/heroku_28jfstht"  
+    //y asi ya no se ve en el github la cadena de conexion a BD
 }
 
 process.env.URLDB = urlDB;
